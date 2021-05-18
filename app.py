48f47c373a3b91564f008d3fda66a513d3c2b96c
TAKAHASHI Shuuji <shuuji3@gmail.com>
2021-05-18 23:50:11 +0900

    Add three routes using variables

--- app.py
+++ app.py
@@ -12,3 +12,21 @@ def index():
 @app.route('/hello')
 def hello():
     return 'Hello, World'
+
+
+@app.route('/user/<username>')
+def show_user_profile(username):
+    """Show the user profile for that user."""
+    return f'User {escape(username)}'
+
+
+@app.route('/post/<int:post_id>')
+def show_post(post_id):
+    """Show the post with the given id, the id is an integer."""
+    return f'Post #{post_id}'
+
+
+@app.route('/path/<path:subpath>')
+def show_subpath(subpath):
+    """Show the subpath after /path"""
+    return f'Subpath {escape(subpath)}'
